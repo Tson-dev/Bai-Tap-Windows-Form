94571f4b96e8f8ef6888c0a30625c2bbdc007c25
Tson-dev <2312739@dlu.edu.vn>
2025-08-30 23:55:38 +0700

    Add files via upload

---
+++ BaiTap01/HangHoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForm
+{
+    public class HangHoa
+    {
+        public int MaHang {  get; set; }
+        public string TenHang { get; set; }
+        public string DVT { get; set; }
+        public uint SoLuong {  get; set; }
+        public float DonGia {  get; set; }
+
+        public HangHoa()
+        {
+            TenHang = string.Empty; 
+            DVT = string.Empty;
+            SoLuong = 0;
+            DonGia = 0;
+        }
+        public string HienThi()
+        {
+            return string.Format("{0}, {1}, {2}, {3}, {4}", MaHang, TenHang, DVT, SoLuong, DonGia);
+        }
+    }
+}
---
+++ BaiTap01/frmBai3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class frmBai3 : Form
+    {
+        public frmBai3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAPlusB_Click(object sender, EventArgs e)
+        {
+            //Chưa tính trường hợp txtA hoặc txtB đang chứa dữ liệu khác chữ số
+            ToanHoc.Plus(int.Parse(txtA.Text), int.Parse(txtB.Text), out int s);
+            lbResult.Text = s.ToString();
+            Reset();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            //Chưa tính trường hợp txtN đang chứa dữ liệu khác chữ số
+            lbResult.Text = ToanHoc.Sum(int.Parse(txtN.Text)).ToString();
+            Reset();
+        }
+        private void Reset()
+        {
+            txtA.Text = string.Empty;
+            txtB.Text = string.Empty;
+            txtN.Text = string.Empty;
+        }
+    }
+}
